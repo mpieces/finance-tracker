1bb1f99b215aca9d6942ee0c53195fc50838db32
Marisa Ramon <mer0522@gmail.com>
2021-07-20 13:23:05 -0500

    Add friends route, show method, and table view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
+  
   def my_portfolio
     @tracked_stocks = current_user.stocks 
+  end
+
+  def show_friends
+    @friends = current_user.friends
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   root "welcome#index"
+
   get "/my_portfolio", to: "users#my_portfolio"
   get "/index", to: "welcome#index"
   get "search_stock", to: "stocks#search"
+  get "/my_friends", to: "users#show_friends"
 end
